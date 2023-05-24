387e1985aa1ed9f0e3d7d5d9deb56db0f8e5a769
Chris Gibbons <gbbns@users.noreply.github.com>
2023-05-24 19:07:33 +0100

    chore: revert file

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/customers/',
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
