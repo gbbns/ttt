22b59fa5d3cca49dc4e1283c986b5e7e99ea58ad
Chris Gibbons <gbbns@users.noreply.github.com>
2023-05-24 13:33:13 +0100

    chore: remove console.log

--- app/customers/page.js
+++ app/customers/page.js
@@ -7,7 +7,6 @@ export const dynamicParams = true;
 
 export default async function Home({ params }) {
   const { page } = params;
-  console.log(page);
   const data = await getData();
   return (
     <>
