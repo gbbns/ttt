17fe77f8755abfdfb117f14bce82a2419722760b
Chris Gibbons <gbbns@users.noreply.github.com>
2023-06-13 12:20:00 +0100

    feat: paginator form in own component

---
+++ components/Paginator.js
@@ -0,0 +1,19 @@
+export default function Paginator() {
+  return (
+    <>
+      <form id='Pagination' className='content__wrapper-form'>
+        <fieldset>
+          <legend>Page results</legend>
+          <label htmlFor='PaginationSelect'>
+            Page
+            <select name='Pagination' id='PaginationSelect'>
+              <option value='1'>1</option>
+              <option value='2'>2</option>
+            </select>
+            of x
+          </label>
+        </fieldset>
+      </form>
+    </>
+  );
+}
