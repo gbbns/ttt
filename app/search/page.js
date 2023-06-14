afe1a07e66847ce44b829aa5ea7d26189547bf8f
Chris Gibbons <gbbns@users.noreply.github.com>
2023-06-14 10:45:45 +0100

    feat: adds in new search results page

---
+++ app/search/page.js
@@ -0,0 +1,12 @@
+'use client';
+import Link from 'next/link';
+
+export default async function Page() {
+  return (
+    <>
+      <main className='content__wrapper customer__wrapper'>
+        <p>This is the search page</p>
+      </main>
+    </>
+  );
+}
