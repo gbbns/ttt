e153b8f51b93fc3ba73f1d4b78f693ee1ddac95e
Chris Gibbons <gbbns@users.noreply.github.com>
2023-06-14 22:29:35 +0100

    feat: new search results page

--- app/search/page.js
+++ app/search/page.js
@@ -1,12 +1,42 @@
 'use client';
+
+import fetchSearchData from '@/lib/fetch-search-data';
+import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import CustomerLink from '@/components/CustomerLink';
+
 export default async function Page() {
+  const searchParams = useSearchParams();
+  const search = searchParams.get('query');
+  const data = await fetchSearchData(search);
+
   return (
     <>
-      <main className='content__wrapper customer__wrapper'>
-        <p>This is the search page</p>
+      <Header />
+      <main className='content__wrapper'>
+        <ul className='data-wrapper'>
+          {data &&
+            data.customers.map((customer, index) => (
+              <li key={index} className='data-wrapper__item'>
+                <Link
+                  key={index}
+                  href={`/customers/${customer.id}`}
+                  className='data-wrapper__link'>
+                  <CustomerLink
+                    name={customer.name}
+                    email={customer.email}
+                    country_code={customer.country_code}
+                    city={customer.city}
+                  />
+                </Link>
+              </li>
+            ))}
+        </ul>
       </main>
+      <Footer />
     </>
   );
 }
